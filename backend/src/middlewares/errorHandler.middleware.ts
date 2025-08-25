991b88ddb2b01e8ccec4e50d145313e287c2309a
htung0403 <htung0403@gmail.com>
2025-08-26 01:22:25 +0700

    feat: enhance error handling middleware with Zod error formatting

--- backend/src/middlewares/errorHandler.middleware.ts
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -1,10 +1,40 @@
-import { ErrorRequestHandler } from "express";
+import { ErrorRequestHandler, Response } from "express";
 import { HTTPSTATUS } from "../config/http.config";
+import { AppError } from "../utils/app-error";
+import { ZodError } from "zod";
+import { ErrorCodeEnum } from "../enums/error-code.enum";
 
-export const errorHandler: ErrorRequestHandler = (err, req, res, next): any => {
-  console.log("Error occurred on PATH:", req.path);
+const formatZodError = (res: Response, error: ZodError) => {
+  const errors = error?.issues?.map((error) => ({
+    field: error.path.join("."),
+    message: error.message,
+  }));
+
+  return res.status(HTTPSTATUS.BAD_REQUEST).json({
+    message: "Validation error",
+    errors: errors,
+    errorCode: ErrorCodeEnum.VALIDATION_ERROR,
+  });
+};
+export const errorHandler: ErrorRequestHandler = (
+  error,
+  req,
+  res,
+  next
+): any => {
+  console.log("Error occurred on PATH:", req.path, "Error:", error);
+
+  if (error instanceof ZodError) {
+    return formatZodError(res, error);
+  }
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+      error: error,
+    });
+  }
   res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
     message: "Something went wrong",
-    error: err.message,
+    error: error.message,
   });
 };
