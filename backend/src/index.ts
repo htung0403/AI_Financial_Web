0b035b4daaeb8b14b3906ca1886e7e8ee9d858bf
htung0403 <htung0403@gmail.com>
2025-08-26 03:59:55 +0700

    feat: implement user routes and controllers for retrieving current user information with JWT authentication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,25 @@
 import "dotenv/config";
+import "./config/passport.config";
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
+import passport from "passport";
 import { Env } from "./config/env.config";
 import { HTTPSTATUS } from "./config/http.config";
 import { errorHandler } from "./middlewares/errorHandler.middleware";
 import { asyncHandler } from "./middlewares/asyncHandler.middleware";
 import { BadRequestException } from "./utils/app-error";
 import authRoutes from "./routes/auth.route";
+import userRoutes from "./routes/user.route";
 import connectDatabase from "./config/database.config";
+import { passportAuthenticateJwt } from "./config/passport.config";
 
 const app = express();
 const BASE_PATH = Env.BASE_PATH;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(passport.initialize());
 
 app.use(
   cors({
@@ -31,6 +37,7 @@ app.get(
 );
 
 app.use(`${BASE_PATH}/auth`, authRoutes);
+app.use(`${BASE_PATH}/user`, passportAuthenticateJwt, userRoutes);
 
 app.use(errorHandler);
 
@@ -40,6 +47,6 @@ app.listen(Env.PORT, async () => {
   if (Env.NODE_ENV === "development") {
     console.log("Running in development mode");
   }
-  
+
   console.log(`Server is running on port ${Env.PORT} in ${Env.NODE_ENV} mode`);
 });
